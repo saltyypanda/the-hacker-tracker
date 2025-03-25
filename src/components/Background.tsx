dc268aeeee5967a68cec909a947ef53e330bab8a
saltyypanda <esh7943@rit.edu>
2025-03-25 01:03:30 -0400

    Made changing background

---
+++ src/components/Background.tsx
@@ -0,0 +1,46 @@
+import { ReactNebula } from "@flodlc/nebula";
+import starrySky from "../assets/starry-sky-bg.png";
+import { useEffect, useState } from "react";
+
+export function Background() {
+  const [opacity, setOpacity] = useState(1);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollY = window.scrollY;
+      const maxScroll = window.innerHeight - 200;
+
+      setOpacity(scrollY > maxScroll ? 0 : 1);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <div className="fixed top-0 left-0 w-full h-full z-[-10] bg-[#121212]">
+      <div
+        className="fixed top-0 left-0 w-full h-full z-[-9] transition-all duration-300"
+        style={{ opacity: opacity }}
+      >
+        <ReactNebula
+          config={{
+            starsRotationSpeed: 2,
+            nebulasIntensity: 15,
+            sunScale: 1,
+            planetsScale: 1,
+            solarSystemOrbite: 80,
+            solarSystemSpeedOrbit: 30,
+          }}
+        />
+      </div>
+      <div
+        className="fixed bottom-0 left-0 w-full h-screen bg-cover bg-center bg-no-repeat z-[-8] transition-all duration-300"
+        style={{
+          backgroundImage: `url(${starrySky})`,
+          opacity: opacity == 1 ? 35 : 0,
+        }}
+      />
+    </div>
+  );
+}
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,28 +1,12 @@
 import { Children, ReactNode } from "react";
-import { ReactNebula } from "@flodlc/nebula";
 import NavBar from "./Navbar";
-import starrySky from "../assets/starry-sky-bg.png";
+import { Background } from "./Background";
 
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
     <html data-theme="space" lang="en">
       <body className="font-inter">
-        <div className="fixed top-0 left-0 w-full h-full z-[-10]">
-          <ReactNebula
-            config={{
-              starsRotationSpeed: 2,
-              nebulasIntensity: 15,
-              sunScale: 1,
-              planetsScale: 1,
-              solarSystemOrbite: 80,
-              solarSystemSpeedOrbit: 30,
-            }}
-          />
-        </div>
-        <div
-          className="fixed bottom-0 left-0 w-full h-screen bg-cover bg-center bg-no-repeat opacity-35 z-[-9]"
-          style={{ backgroundImage: `url(${starrySky})` }}
-        />
+        <Background />
         <NavBar />
         <div className="flex flex-col w-full items-center">
           {Children.map(children, (child, index) => (
