61f4eca7c287485f83659a0f339b801c33e814de
saltyypanda <esh7943@rit.edu>
2025-03-26 20:16:15 -0400

    Changed typing intros

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -3,7 +3,6 @@ export function Footer() {
     <footer className="footer sm:footer-horizontal footer-center bg-base-100 text-base-content p-8">
       <aside>
         <p>
-          {/* Copyright © {new Date().getFullYear()} Esther Hacker. All rights reserved. */}
           © {new Date().getFullYear()} - Made by Esther Hacker 🐼
         </p>
       </aside>
